dff529968473c15ff1024dd33b697b97cb207025
Damien Mauchamp <d.mauchamp@login-info.com>
2023-11-18 19:28:09 +0100

    Displaying demo albums

---
+++ components/LibraryAlbum/LibraryAlbum.tsx
@@ -0,0 +1,51 @@
+import classes from './LibraryAlbum.module.css'
+import Image from 'next/image';
+
+type LibraryAlbum = {
+	id: string;
+	type: string; // 'library-albums'
+	href: string;
+	attributes: LibraryAlbumAttributes;
+}
+
+type LibraryAlbumAttributes = {
+	trackCount: number;
+	genreNames: string[];
+	releaseDate: string;
+	name: string;
+	artistName: string;
+	artwork: LibraryAlbumArtwork;
+	playParams: object;
+	dateAdded: string;
+}
+
+type LibraryAlbumArtwork = {
+	width: number;
+	height: number;
+	url: string;
+}
+
+const LibraryAlbum = (props: {album: LibraryAlbum, displayedAlbumId: number}) => {
+
+	const defaultSize = 500;
+
+	const getImageUrl = (album: LibraryAlbum, size:number = defaultSize) => {
+		return album.attributes.artwork.url
+			.replace('{w}', String(size))
+			.replace('{h}', String(size));
+	}
+
+	return (
+		<>
+			<div>
+				<Image className='' src={getImageUrl(props.album, defaultSize)} alt={`${props.album.attributes.name} by ${props.album.attributes.artistName}`}
+					width={defaultSize} height={defaultSize} />
+				<p>ID: {props.displayedAlbumId}</p>
+				<p>{props.album.attributes.name}</p>
+				<p>{props.album.attributes.artistName}</p>
+			</div>
+		</>
+	)
+}
+
+export default LibraryAlbum
--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        // hostname: '**.mzstatic.com',
+        // hostname: 'is1-ssl.mzstatic.com',
+        // hostname: 'is2-ssl.mzstatic.com',
+        // hostname: 'is3-ssl.mzstatic.com',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
